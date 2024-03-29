ef823dd2d796918332a34199baeaa019e9c1b5b6
Carlie-Dev <ellepheonixgames@gmail.com>
2024-03-29 08:23:23 -0500

    buit calc file

---
+++ calc.py
@@ -0,0 +1,15 @@
+class calculator():
+    def __init__(self,value):
+        self.value = value
+
+    def add(num1,num2):
+        return num1 + num2
+    
+    def subtract(num1, num2):
+        return num1 - num2
+    
+    def multiply(num1,num2):
+        return num1*num2
+    
+    def divide(num1,num2):
+        return num1/num2
