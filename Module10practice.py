90de3c2f03ab2594dce3204252f07400fae744a6
Carlie-Dev <ellepheonixgames@gmail.com>
2024-03-29 08:52:09 -0500

    Getting table headers

--- Module10practice.py
+++ Module10practice.py
@@ -1,8 +1,42 @@
-import calc
+#import calc
+# from calc import calculator
+# my_num1 = 10
+# my_num2 = 15
 
-my_num1 = 10
-my_num2 = 15
+# #prints 25
+# print(calculator.add(my_num1,my_num2))
 
-#prints 25
-print(calc.calculator.add(my_num1,my_num2))
+#Importing library
+import requests
+from bs4 import BeautifulSoup
 
+#Set url of page
+url = 'https://en.wikipedia.org/wiki/Python_(programming_language)'
+
+#Send a get request to the webpage
+response = requests.get(url)
+
+#Parsing the html of the webpage
+soup = BeautifulSoup(response.text)
+
+#Find the hyper links with the text and replace the link with the text
+for a_tag in soup.find_all('a'):
+    a_tag.replace_with(a_tag.text)
+
+tables = soup.find_all('table')
+if len(tables) > 1:
+    table = tables[1]
+else:
+    table = tables[0]
+
+#initialize list to hold straight data
+table_data = []
+
+#extract table headers and add to list
+headers = []
+for header in table.find_all('th'):
+    headers.append(header.text.strip())
+
+table_data.append(headers)
+
+#Extracting rows from the table
